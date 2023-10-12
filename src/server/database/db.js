ee90b4ee426ae5dd2e8a360ed3cd32dc5a13e18a
Diogo <you@example.com>
2023-10-12 20:27:39 -0300

    .env install

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const conn= ()=>{
     console.log("Waiting for connection...")
     
-    mongoose.connect("mongodb+srv://admin:1234@cluster0.njvl2nk.mongodb.net/?retryWrites=true&w=majority", { 
+    mongoose.connect( process.env.DB_URI, { 
         useNewUrlParser: true, 
         useUnifiedTopology: true 
     })
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,13 @@
 import express from "express"
 import conn from "./database/db.js"
 import Router from "./routes/user.route.js"
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 conn()
 
 app.use(express.json())
